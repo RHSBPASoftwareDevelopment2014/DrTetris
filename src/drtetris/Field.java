d1aec85d353ece8cda7678815e65c12ed517613e
unknown <13753@HSC21116.rcs.k12.in.us>
2014-09-18 09:51:25 -0400

    Removed Logging

--- src/drtetris/Field.java
+++ src/drtetris/Field.java
@@ -12,10 +12,8 @@ public class Field extends TileMap {
     }
     
     public void addMap(Tile[][] map, int x, int y) {
-        System.out.println(x + " " + y);
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[i].length; j++) {
-                System.out.println((y + i) + " " + (x + j));
                 if(map[i][j] != null) {
                     this.map[y + i][x + j] = map[i][j];
                 }
