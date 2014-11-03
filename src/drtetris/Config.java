de28599207a11ed9a65a45299bdeafb0341a4bb3
Spencer Lanman <spencer@scanpower.com>
2014-11-03 09:58:21 -0500

    Added Config file

---
+++ src/drtetris/Config.java
@@ -0,0 +1,16 @@
+
+package drtetris;
+
+public class Config {
+    public static final double BASESPEED = .16;
+    public static final double SPEEDINCREMENT = .1;
+    
+    public static final int FIELDOFFSET = 10;
+    public static final int FIELDX = 150;
+    public static final int FIELDY = 90;
+    public static final int BLOCKDELAY = 400;
+    
+    public static final String GAMEBACKGROUND = "res/gamebackground.png";
+    public static final String PAUSESCREEN = "res/pausedscreen.png";
+    public static final String GAMEOVERSCREEN = "res/gameover.png";
+}
