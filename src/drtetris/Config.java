86c0ab34c55c697efcb4d6bd24e5e93efe078509
Spencer Lanman <spencer@scanpower.com>
2014-11-13 21:11:36 -0500

    Added build.xml

--- src/drtetris/Config.java
+++ src/drtetris/Config.java
@@ -7,7 +7,7 @@ public class Config {
     public static final double SPEEDINCREMENT = .04;
     public static final double SPEEDXINCREMENT = .005;
     public static final double STACKTOLERANCE = 12;
-    public static final double PARTICLESTRAY = 2;
+    public static final double PARTICLESTRAY = 0;
     public static final double SPEEDLIMIT = 1;
     public static final double SPEEDXLIMIT = .1;
     
@@ -18,7 +18,7 @@ public class Config {
     public static final int FIELDY = 90;
     public static final int BLOCKDELAY = 400;
     public static final int BLOCKSIZE = 40;
-    public static final int TILEPARTICLESIZE = 5;
+    public static final int TILEPARTICLESIZE = 40;
     public static final int XMOVEDELAY = 200;
     public static final int XMOVEDELAYDECREMENT = 5;
     
@@ -27,5 +27,5 @@ public class Config {
     public static final String GAMEOVERSCREEN = "res/gameover.png";
     public static final String DIRTTILE = "res/Dirt2.png";
     public static final String SAPPHIRETILE = "res/Sapphire4.png";
-    public static final String TUNNELTILE = "res/spencersbluetunnel.png";
+    public static final String TUNNELTILE = "res/bluetunnel.png";
 }
